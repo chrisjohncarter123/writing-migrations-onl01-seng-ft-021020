0dd3a6423846dc4bb14db57d2532f0ad6f908984
Christopher Carter <chrisjohncarter123@gmail.com>
2020-03-19 01:24:44 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,6 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   
   def change
-    add_column :grade, :birthdate, :string
+    add_column :students, :grade, :Integer
   end
 end
