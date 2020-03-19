a08e7180eb6c31e9b583052574e5e49e4b9647ce
Christopher Carter <chrisjohncarter123@gmail.com>
2020-03-19 01:23:23 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,6 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   
   def change
-    add_column :grade, :favorite_food, :string
+    add_column :grade, :birthdate, :string
   end
 end
